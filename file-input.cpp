8eb0e1c1c8a0d279b266d703b8fb5563642edb78
Rob Hess <robwhess@gmail.com>
2019-01-16 13:13:57 -0800

    Add code from 1/16 lecture.

--- file-input.cpp
+++ file-input.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 
+#include <fstream>
+
 struct student {
   std::string name;
   float gpa;
 };
 
 int main(int argc, char const *argv[]) {
+
+  // for (int i = 0 ; i < argc; i++) {
+  //   std::cout << "argv[ " << i << "]: " << argv[i] << std::endl;
+  // }
+
+  std::ifstream infile;
+  infile.open("./testdata.txt");
+  if (infile.fail()) {
+    std::cerr << "Failed to open testdata.txt" << std::endl;
+    return 1;
+  }
+
+  int n;
+  infile >> n;
+  std::cout << "n: " << n << std::endl;
+
+  infile.close();
 
   return 0;
 }
